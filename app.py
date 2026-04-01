ab5c36d1f76c6dce6c09720c91555ae201b9e6b7
Abi-abi22 <abinobi6@gmail.com>
2026-04-01 12:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ try:
     genai.configure(api_key=API_KEY)
     
     # Standard Flash Model (Nichayamah work aagum)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-pro')
 except Exception as e:
     st.error(f"Setup Error: {str(e)}")
 
